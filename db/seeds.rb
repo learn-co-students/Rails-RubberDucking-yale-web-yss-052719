3582fc5a8771b5656abdcec60bb0c72dfcb0c558
Leonie Dong <leoniedong@hotmail.com>
2019-06-27 18:03:27 -0400

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
+Student.destroy_all
+Duck.destroy_all
+
 10.times do
   Student.create(name: Faker::Name.name, mod: rand(1..5))
 end
 
-5.times do
-  Duck.create(name: Faker::Dog.meme_phrase, description: Faker::Dog.breed)
+25.times do
+  Duck.create(name: Faker::Dog.meme_phrase, description: Faker::Dog.breed, student: Student.all.sample)
 end
